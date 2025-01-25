da38fb1b5c8906b9de4d604a470a684a52215ba7
Головин Данила <golovindanila340@gmail.com>
2025-01-25 18:56:49 +0300

    Edit tabulation

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var first = Convert.ToDouble(args[0]);
+		var first = Convert.ToDouble(args[0]);
 		var second = Convert.ToDouble(args[1]);
 		
 		Console.WriteLine(Math.Sqrt( first + second));
